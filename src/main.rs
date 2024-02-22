5cd87107c986b2eaf62c975df44b0ea783b824e7
Sarat Chandra <me@saratchandra.in>
2024-02-22 15:09:27 +0530

    feat: Add flag for config.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use gumdrop::Options;
 use miette::Result;
 use tracing_subscriber::EnvFilter;
 
@@ -5,14 +6,25 @@ mod agent;
 mod cfg;
 mod forwarder;
 
+#[derive(Debug, Options)]
+struct Args {
+    #[options(help = "print help message")]
+    help: bool,
+
+    #[options(help = "configuration path")]
+    config: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // Initialize tracing fmt subscriber with level from env.
     let filter = EnvFilter::from_default_env();
     tracing_subscriber::fmt().with_env_filter(filter).init();
 
+    let opts = Args::parse_args_default_or_exit();
+
     // Parse config.
-    let cfg = cfg::parse_config("config.json").await?;
+    let cfg = cfg::parse_config(&opts.config.unwrap_or("config.json".to_string())).await?;
 
     match cfg.mode {
         cfg::Mode::Agent => {
